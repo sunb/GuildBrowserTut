9faed6f6587ff2bccc487fa43a46dbccaa1d54bc
Tran Xuan Bach <sunb258@gmail.com>
2012-09-27 00:22:53 +0700

    - add a new test case - update project setting

---
+++ GuildBrowserLogicTests/WowUtilsTests.h
@@ -0,0 +1,13 @@
+//
+//  WowUtilsTests.h
+//  GuildBrowser
+//
+//  Created by Tran Xuan Bach on 9/27/12.
+//  Copyright (c) 2012 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WowUtilsTests : SenTestCase
+
+@end
---
+++ GuildBrowserLogicTests/WowUtilsTests.m
@@ -0,0 +1,20 @@
+//
+//  WowUtilsTests.m
+//  GuildBrowser
+//
+//  Created by Tran Xuan Bach on 9/27/12.
+//  Copyright (c) 2012 Charlie Fulton. All rights reserved.
+//
+
+#import "WowUtilsTests.h"
+#import "WoWUtils.h"
+
+@implementation WowUtilsTests
+
+- (void) testCharacterClassNameLookup{
+    STAssertEqualObjects(@"Warrior", [WoWUtils classFromCharacterType:1], @"Class Type Should Be Warrior");
+    STAssertFalse([@"Mage" isEqualToString:[WoWUtils classFromCharacterType:2]], nil);
+    STAssertTrue([@"Paladin" isEqualToString:[WoWUtils classFromCharacterType:2]], nil);
+}
+
+@end
